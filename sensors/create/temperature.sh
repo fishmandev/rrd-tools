05280914e78f674b7bfd544ce5d3bdb3527607c8
Dmitriy Fishman <fishman.code@gmail.com>
2021-07-19 12:55:51 +0300

    Changed resolutions: 1 week - 1m resolution 1 month - 5m resolution 3 month - 15m resolution 6 week - 30m resolution 1 year - 1h resolution

--- sensors/create/temperature.sh
+++ sensors/create/temperature.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
 
-rrdtool create temperatures.rrd \
+rrdtool create temperatures_tmp.rrd \
 --step 60 \
 DS:temp:GAUGE:120:0:50 \
-RRA:MAX:0.5:1:720 \
-RRA:MAX:0.5:5:288 \
-RRA:MAX:0.5:15:672 \
-RRA:MAX:0.5:60:720
+RRA:MAX:0.5:1:10080 \
+RRA:MAX:0.5:5:8640 \
+RRA:MAX:0.5:15:8640 \
+RRA:MAX:0.5:30:8640 \
+RRA:MAX:0.5:60:8640
